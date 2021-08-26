a054b1c54a737339a7f1108915931f39e77826fa
gauravjangid45 <gauravjangidamer@gmail.com>
2021-08-26 23:21:05 +0530

    TASK 11 FROM SUMMER TRAINING

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:realapp2/app_screens/splash_screen.dart';
+// import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SPLASHSCREEN(),
+    ),
+  );
+}
